063e3ad61bdc18d30c5ee6980bd5b7e4e9be7e78
Pawel P <pawel.pppp@live.com>
2018-09-11 18:16:00 +0200

    Add project files.

---
+++ Vidly/Controllers/MovieController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie/Random
+        public ActionResult Random()
+        {
+            var move = new Movie() { Name = "Shrek" };
+            return View(move);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("## Id = " + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (pageIndex == null)
+                pageIndex = 1;
+            if (string.IsNullOrEmpty(sortBy))
+            {
+                sortBy = "name";
+            }
+            return Content(string.Format("pageIndex= {0} &sortBy= {1}", pageIndex, sortBy));
+        }
+    }
+}
